f45d59c98f2d976da46aceb8ebf491d5b04fe8b5
Douglas Reis <douglasdosreiscouto@gmail.com>
2025-05-12 21:19:46 -0300

    Incrementando um estudo de caso

---
+++ EstudoCaso1/Aluno.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudoCaso1
+{
+    class Aluno
+    {
+        public string nome, curso;
+        public int matricula, dtNascimento, idade;
+        public string CadastroAluno(String nome, int matricula, int dtNascimento, string curso)
+        {
+            this.nome = nome;
+            this.matricula = matricula;
+            this.dtNascimento = dtNascimento;
+            this.curso = curso;
+            return "Cadastro realizado com sucesso!";
+        }
+    }
+    class Calculadora
+    {
+        public int nota1, nota2, nota3, dtNascimento;
+        public int anoAtual = DateTime.Now.Year;
+        public int CalcularIdade(int dtNascimento, int anotAtual)
+        {
+            return anoAtual - dtNascimento;
+        }
+        public int CalculaMedia(int nota1, int nota2, int nota3)
+        {
+            int media = (nota1 + nota2 + nota3) / 3;
+            return media;
+        }
+    }
+}
---
+++ EstudoCaso1/Program.cs
@@ -0,0 +1,16 @@
+﻿using EstudoCaso1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Aluno aluno1 = new Aluno();
+        Calculadora aluno2 = new Calculadora();
+        
+        Console.WriteLine(aluno1.CadastroAluno("Douglas", 100, 1997, "ADS"));  
+        Console.WriteLine(aluno2.CalculaMedia(10, 4, 8));    
+
+
+
+    }
+}
