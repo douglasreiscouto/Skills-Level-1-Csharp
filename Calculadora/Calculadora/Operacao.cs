c4630af8855a8dc3da36b34939fda507afaf6a31
Douglas Reis <douglasdosreiscouto@gmail.com>
2025-05-28 13:56:24 -0300

    Ajuste a classe de operacao

--- Calculadora/Calculadora/Operacao.cs
+++ Calculadora/Calculadora/Operacao.cs
@@ -8,9 +8,62 @@ namespace Calculadora
 {
     internal class Operacao
     {
-        float Soma(float fValorUm, float fValorDois)
+        int iOpeMenu;
+        float fValorUm, fValorDois, fResultado;
+        public void SolicitaValor() 
         {
-            return fValorUm + fValorDois;
+            Console.WriteLine();
+            Console.Write("Digite o Primeiro Valor: ");
+            fValorUm = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Segundo Valor: ");
+            fValorDois = float.Parse(Console.ReadLine());
+        }
+        public void MenuCalculadora()
+        {
+            Console.WriteLine("****MENU DE OPERAÇÕES****");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtrair");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+            Console.WriteLine("0 - Sair");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("Digite o número da operação desejada: ");
+            iOpeMenu = int.Parse(Console.ReadLine());
+        }
+
+        public void RetornaResultado(float fResultado, int iOpeMenu)
+        {
+            if (iOpeMenu == 1)
+            {
+                Console.Write("RESULTADO DA SOMA: " + fResultado);
+            }
+            else if (iOpeMenu == 2)
+            {
+                Console.Write("RESULTADO DA SUBTRAÇÃO: " + fResultado);
+            }
+            else if (iOpeMenu == 3) 
+            {
+                Console.Write("RESULTADO DA MULTIPLICAÇÃO: " + fResultado);
+            }
+            else if (iOpeMenu == 4)
+            {
+                Console.Write("RESULTADO DA DIVISÃO: " + fResultado);
+            }
+            else if (iOpeMenu == 0)
+            {
+                Operacao menu = new Operacao();
+                menu.MenuCalculadora();
+            }
+        }
+        public void Soma()
+        {
+            Operacao soma = new Operacao();
+
+            fResultado = fValorUm + fValorDois;
+            soma.RetornaResultado(fResultado, 1);
+
         }
         float Subtrair(float fValorum, float fValorDois)
         {
@@ -24,5 +77,6 @@ namespace Calculadora
         {
             return fValorUm / fValordois;
         }
+
     }
 }
--- Calculadora/Calculadora/Program.cs
+++ Calculadora/Calculadora/Program.cs
@@ -1,27 +1,13 @@
-﻿internal class Program
+﻿using Calculadora;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        int iOpeMenu;
-        float fValorUm, fValorDois, fResultado;
+        int iOpeMenu; 
 
-        Console.WriteLine("****MENU DE OPERAÇÕES****");
-        Console.WriteLine("1 - Soma");
-        Console.WriteLine("2 - Subtrair");
-        Console.WriteLine("3 - Multiplicação");
-        Console.WriteLine("4 - Divisão");
-        Console.WriteLine("0 - Sair");
-        Console.WriteLine();
-        Console.WriteLine();
-        Console.Write("Digite o número da operação desejada: ");
-        iOpeMenu = int.Parse(Console.ReadLine());
-
-        //Console.WriteLine("Digite o Primeiro Valor: ");
-        //fValorUm = float.Parse(Console.ReadLine());
-
-        //Console.WriteLine("Digite o Segundo Valor: ");
-        //fValorDois = float.Parse(Console.ReadLine());
-
-
+        Operacao operacao = new Operacao();
+        operacao.MenuCalculadora();
+        operacao.SolicitaValor();
     }
 }
